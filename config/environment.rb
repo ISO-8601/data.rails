ddc780c7784802e200ef16045219cf2a393a471b
Andrew Davis <papyromancer@lem.(none)>
2009-05-02 12:55:09 -0500

    added Amazon Simple DB tools

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,4 @@
+
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
@@ -19,6 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+
+  config.gem 'aws-sdb'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
